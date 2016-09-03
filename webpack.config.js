e2bd5686c04c2f2f0a056786103f8fef4eee836a
Rich Churcher <rich.churcher@gmail.com>
2016-09-03 16:50:09 +1200

    Let's try the right prop

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'src', 'public'),
-    file: 'bundle.js'
+    filename: 'bundle.js'
   },
   module: {
     loaders: [
