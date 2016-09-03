6b831ab4a935e162b93d723631969b0b259a9d23
Rich Churcher <rich.churcher@gmail.com>
2016-09-03 16:41:18 +1200

    Playing with dirname

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   entry: './src/index.js',
-  output: './src/public/bundle.js',
+  output: {
+    path: `${__dirname}src/public/`,
+    file: 'bundle.js'
+  },
   module: {
     loaders: [
       {loader: 'babel-loader?cacheDirectory', test: /\.jsx?$/, exclude: /node_modules/}
